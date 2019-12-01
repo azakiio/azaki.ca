7de5745341b31fe7d028dc162fc9654ef64c0d54
azakiio <azakica@gmail.com>
2019-11-30 21:03:48 -0400

    testing csgomodel

--- csgomodel/prediction.js
+++ csgomodel/prediction.js
@@ -1,40 +1,20 @@
 function solve() {
 
+  var link = document.getElementById("link").value;
 
-  //var result = this.responseText.split(" ");
-  var puzzle = document.getElementById("puzzle");
-  var data = "";
-  for (var index = 0; index < 81; index++) {
-    var row = Math.floor(index / 9);
-    var col = index % 9;
-    data += (puzzle.rows[row].cells[col].children[0].value != "") ? puzzle.rows[row].cells[col].children[0].value : "0";
-  }
+  var data = "{\"match\":\"https://www.hltv.org/matches/2337794/liquid-vs-fnatic-ecs-season-8-finals\"}";
 
   var xhr = new XMLHttpRequest();
-
-  xhr.addEventListener("readystatechange", function () {
-    if (this.readyState === 4) {
-      var result = this.responseText.split(" ");
-      if (result.length != 82) {
-        document.getElementById("error").innerHTML += "Sudoku is invalid. Please confirm input<br>"
-        return;
-      }
-      var table = document.getElementById("puzzle");
-      for (var index = 0; index < 81; index++) {
-        var row = Math.floor(index / 9);
-        var col = index % 9;
-        table.rows[row].cells[col].children[0].value = result[index];
-      }
-      document.getElementById("error").innerHTML = "";
-
-
-
+  xhr.withCredentials = true;
+  
+  xhr.addEventListener("readystatechange", function() {
+    if(this.readyState === 4) {
+      console.log(this.responseText);
     }
   });
-
-
-  xhr.open("POST", "https://us-central1-azakica.cloudfunctions.net/sudoku");
-  xhr.setRequestHeader("Content-Type", "text/plain");
-
+  
+  xhr.open("POST", "https://us-central1-azakica.cloudfunctions.net/csgo_predict");
+  xhr.setRequestHeader("Content-Type", "application/json");
+  
   xhr.send(data);
 }
